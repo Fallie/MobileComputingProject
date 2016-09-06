3c1f30be98cc9f80d5915c2402161d6793d5c31c
Ziyuan Wang <ziyuan.wang92@gmail.com>
2016-09-06 15:37:49 +1000

    Implemented swip views left and right.
    Added three fragments for camera, chatlist and discorver,
    Could swipe views left and right.
    tabs are shown as textView, should be changed to image later.

--- app/src/main/java/com/example/wyyz/snapchat/activity/MainActivity.java
+++ app/src/main/java/com/example/wyyz/snapchat/activity/MainActivity.java
@@ -1,19 +1,98 @@
 package com.example.wyyz.snapchat.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.wyyz.snapchat.R;
 import com.example.wyyz.snapchat.db.SnapChatDB;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+
+public class MainActivity extends FragmentActivity implements View.OnClickListener {
 
     private SnapChatDB snapChatDB;
+
+    private ViewPager pager ;
+    private ArrayList<Fragment> fragments;
+    private PagerAdapter adapter;
+    private TextView tv_tab0, tv_tab1, tv_tab2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         snapChatDB = SnapChatDB.getInstance(this);
+
+        initView();
     }
+
+    private void initView(){
+        fragments=new ArrayList<Fragment>();
+        fragments.add(new ChatlistFragment());
+        fragments.add(new CameraFragment());
+        fragments.add(new DiscoverFragment());
+        adapter = new MyPagerAdapter(getSupportFragmentManager(), fragments);
+        pager=(ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        tv_tab0=(TextView) findViewById(R.id.tv_tab0);
+        tv_tab1=(TextView) findViewById(R.id.tv_tab1);
+        tv_tab2=(TextView) findViewById(R.id.tv_tab2);
+        pager.setCurrentItem(1);
+        tv_tab0.setTextColor(Color.BLACK);
+        tv_tab1.setTextColor(Color.RED);
+        tv_tab2.setTextColor(Color.BLACK);
+
+        tv_tab0.setOnClickListener(this);
+        tv_tab1.setOnClickListener(this);
+        tv_tab2.setOnClickListener(this);
+        pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            public void onPageSelected(int position){
+                switch (position){
+                    case 0:
+                        tv_tab0.setTextColor(Color.RED);
+                        tv_tab1.setTextColor(Color.BLACK);
+                        tv_tab2.setTextColor(Color.BLACK);
+                        break;
+                    case 1:
+                        tv_tab0.setTextColor(Color.BLACK);
+                        tv_tab1.setTextColor(Color.RED);
+                        tv_tab2.setTextColor(Color.BLACK);
+                        break;
+                    case 2:
+                        tv_tab0.setTextColor(Color.BLACK);
+                        tv_tab1.setTextColor(Color.BLACK);
+                        tv_tab2.setTextColor(Color.RED);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.tv_tab0:
+                pager.setCurrentItem(0);
+                break;
+            case R.id.tv_tab1:
+                pager.setCurrentItem(1);
+                break;
+            case R.id.tv_tab2:
+                pager.setCurrentItem(2);
+                break;
+            default:
+                break;
+        }
+    }
+
 }
