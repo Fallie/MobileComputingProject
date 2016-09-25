11707b063e6694287ef566cf1d3ef3f741d7f871
Ziyuan Wang <ziyuan.wang92@gmail.com>
2016-09-26 00:30:30 +1000

    settings layout

--- app/src/main/java/com/example/wyyz/snapchat/activity/ProfileEditingActivity.java
+++ app/src/main/java/com/example/wyyz/snapchat/activity/ProfileEditingActivity.java
@@ -1,15 +1,130 @@
 package com.example.wyyz.snapchat.activity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.wyyz.snapchat.R;
 
+import java.util.Calendar;
+
 public class ProfileEditingActivity extends AppCompatActivity {
+    public static final int USERNAME=1;
+    public static final int BIRTHDAY=2;
+    public static final int MOBILE=3;
+    public static final int PASSWORD=4;
+    TextView title;
+    Button save;
+    EditText editText;
+    TextView tvCurrentPwd;
+    TextView tvNewPwd;
+    EditText newPwd;
+    TextView tvConfirmPwd;
+    EditText confirmPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_editing);
+        title=(TextView)findViewById(R.id.tv_title);
+        save=(Button)findViewById(R.id.btn_save);
+        editText=(EditText)findViewById(R.id.edit_text1);
+        Intent intent=getIntent();
+        int item=intent.getIntExtra("item",1);
+        switch (item){
+            case USERNAME:
+                title.setText("Email");
+                saveUsername();
+                break;
+            case BIRTHDAY:
+                title.setText("Birthday");
+                saveBirthday();
+                break;
+            case MOBILE:
+                title.setText("Mobile Number");
+                saveMobile();
+                break;
+            case PASSWORD:
+                title.setText("Password");
+                savePassword();
+                break;
+            default:
+                break;
+        }
     }
-}
+
+    private void saveUsername(){
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //update database
+                editText.setText("UserNameSAVED!!!!!");//used for testing
+            }
+        });
+
+    }
+    private void saveBirthday() {
+
+        editText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Calendar c = Calendar.getInstance();
+                new DatePickerDialog(ProfileEditingActivity.this, new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        // TODO Auto-generated method stub
+                        editText.setText(year + "/" + (monthOfYear + 1) + "/" + dayOfMonth);
+                    }
+                }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH)).show();
+
+            }
+        });
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //update database
+                editText.setText("UserNameSAVED!!!!!");
+            }
+        });
+    }
+    private void saveMobile(){
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //update database
+                editText.setText("MobileSAVED!!!!!");
+            }
+        });
+    }
+    private void savePassword(){
+        tvCurrentPwd=(TextView)findViewById(R.id.tv_currentpwd);
+        tvCurrentPwd.setVisibility(View.VISIBLE);
+        tvNewPwd=(TextView)findViewById(R.id.tv_newpwd);
+        tvNewPwd.setVisibility(View.VISIBLE);
+        newPwd=(EditText)findViewById(R.id.et_newpwd);
+        newPwd.setVisibility(View.VISIBLE);
+        tvConfirmPwd=(TextView)findViewById(R.id.tv_confirmpwd);
+        tvConfirmPwd.setVisibility(View.VISIBLE);
+        confirmPwd=(EditText)findViewById(R.id.et_confirmpwd);
+        confirmPwd.setVisibility(View.VISIBLE);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //check password
+                //update database
+                editText.setText("PasswordSAVED!!!!!");
+            }
+        });
+    }
+    }
+
--- app/src/main/java/com/example/wyyz/snapchat/activity/SettingsActivity.java
+++ app/src/main/java/com/example/wyyz/snapchat/activity/SettingsActivity.java
@@ -1,15 +1,61 @@
 package com.example.wyyz.snapchat.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.example.wyyz.snapchat.R;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener{
+    RelativeLayout email;
+    RelativeLayout username;
+    RelativeLayout birthday;
+    RelativeLayout mobile;
+    RelativeLayout password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        email=(RelativeLayout)findViewById(R.id.layout_email);
+        username=(RelativeLayout)findViewById(R.id.layout_name);
+        birthday=(RelativeLayout)findViewById(R.id.layout_birthday);
+        mobile=(RelativeLayout)findViewById(R.id.layout_mobile);
+        password=(RelativeLayout)findViewById(R.id.layout_password);
+        email.setOnClickListener(this);
+        username.setOnClickListener(this);
+        birthday.setOnClickListener(this);
+        mobile.setOnClickListener(this);
+        password.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.layout_name:
+                Intent intent2=new Intent(SettingsActivity.this,ProfileEditingActivity.class);
+                intent2.putExtra("item",ProfileEditingActivity.USERNAME);
+                startActivity(intent2);
+                break;
+            case R.id.layout_birthday:
+                Intent intent3=new Intent(SettingsActivity.this,ProfileEditingActivity.class);
+                intent3.putExtra("item",ProfileEditingActivity.BIRTHDAY);
+                startActivity(intent3);
+                break;
+            case R.id.layout_mobile:
+                Intent intent4=new Intent(SettingsActivity.this,ProfileEditingActivity.class);
+                intent4.putExtra("item",ProfileEditingActivity.MOBILE);
+                startActivity(intent4);
+                break;
+            case R.id.layout_password:
+                Intent intent5=new Intent(SettingsActivity.this,ProfileEditingActivity.class);
+                intent5.putExtra("item",ProfileEditingActivity.PASSWORD);
+                startActivity(intent5);
+                break;
+            default:
+                break;
+        }
     }
 }
