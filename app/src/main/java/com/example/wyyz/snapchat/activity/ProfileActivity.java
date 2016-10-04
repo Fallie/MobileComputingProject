052c00172726783539dcca03286556aa67bdfdde
Ziyuan Wang <ziyuan.wang92@gmail.com>
2016-10-04 23:03:08 +1100

    display qrcode

--- app/src/main/java/com/example/wyyz/snapchat/activity/ProfileActivity.java
+++ app/src/main/java/com/example/wyyz/snapchat/activity/ProfileActivity.java
@@ -1,8 +1,11 @@
 package com.example.wyyz.snapchat.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
@@ -21,6 +24,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     TextView addfriends;
     TextView addedme;
     TextView myfriends;
+    ImageView snapCode;
     FirebaseUser user;
     User currentUser;
     SnapChatDB db;
@@ -43,12 +47,19 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         addedme.setOnClickListener(this);
         myfriends=(TextView)findViewById(R.id.tv_myfriends);
         myfriends.setOnClickListener(this);
+
+        //QRcode
+        snapCode=(ImageView)findViewById(R.id.imgv_snapcode);
     }
 
     protected void onResume(){
         super.onResume();
         currentUser=db.findUserByEmail(user.getEmail());
         _nickName.setText(currentUser.getUsername());
+        String codeStr=currentUser.getQRcode();
+        byte[] bytes= Base64.decode(codeStr,Base64.DEFAULT);
+        Bitmap myBitmap= BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        snapCode.setImageBitmap(Bitmap.createScaledBitmap(myBitmap, 300, 300, false));
     }
 
     @Override
