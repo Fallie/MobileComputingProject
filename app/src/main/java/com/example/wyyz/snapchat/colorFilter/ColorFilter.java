e6fb7ff63b122d8ca44a28b84816b295b8accba7
Qianwen Zhang <Qianwen Zhang>
2016-10-17 23:42:43 +1100

    add two filters: gray and colorful

---
+++ app/src/main/java/com/example/wyyz/snapchat/colorFilter/ColorFilter.java
@@ -0,0 +1,39 @@
+package com.example.wyyz.snapchat.colorFilter;
+
+/**
+ * Created by Fallie on 17/10/2016.
+ */
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+
+public class ColorFilter {
+    public static Bitmap changeToColor(Bitmap bitmap) {
+
+        int width, height;
+        width = bitmap.getWidth();
+        height = bitmap.getHeight();
+
+        Bitmap grayBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(grayBitmap);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+
+
+        float[] array = {1.438f, -0.062f, -0.062f, 0, 0,
+                -0.122f, 1.378f, -0.122f, 0, 0,
+                -0.016f, -0.016f, 1.483f, 0, 0,
+                -0.03f, 0.05f, -0.02f, 1, 0};
+        ColorMatrix colorMatrix = new ColorMatrix(array);
+
+        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(colorMatrix);
+
+        paint.setColorFilter(filter);
+        canvas.drawBitmap(bitmap, 0, 0, paint);
+
+        return grayBitmap;
+    }
+}
